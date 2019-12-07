d433523bfcdb2a77302ab2a5eb6689a32837ceee
sophiasagan <sophiabraddock82@gmail.com>
2019-12-06 21:42:37 -0500

    basic mvp done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,88 @@
 // Your code goes here
+
+// MOUSEOVER 
+const header = document.querySelector('.logo-heading');
+
+    header.addEventListener('mouseover', event => {
+    event.target.style.color = getColor()
+})
+
+function getColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// MOUSEENTER
+
+const busPic = document.querySelector('header img');
+
+busPic.addEventListener('mouseenter', event => {
+    busPic.style.transform = 'scale(1.1)';
+});
+
+// MOUSELEAVE
+
+busPic.addEventListener("mouseleave", () => {
+    busPic.style.transform = "scale(1)";
+})
+
+
+// COPY
+
+document.querySelector(".destination p").addEventListener("copy", thisFunction);
+
+function thisFunction() {
+    alert("Copywritten Text - Do Not Copy!");
+}
+
+// KEYDOWN 
+
+const backgroundChange = document.querySelector('body');
+
+backgroundChange.addEventListener('keydown', (e) => {
+    backgroundChange.style.color = 'hotpink';
+});
+
+// KEYUP
+backgroundChange.addEventListener('keyup', (e) => {
+    backgroundChange.style.color = 'blue';
+});
+
+
+// LOAD 
+
+window.addEventListener('load', function(){
+    alert('Page has loaded. Click ok.');
+})
+
+// RESIZE 
+window.addEventListener('resize', event => {
+    const changeTitle = document.querySelector('.logo-heading');
+    changeTitle.textContent="Get on the Bus!!!"
+});
+
+// CLICK
+
+const  firstTitle = document.querySelector('.text-content *');
+
+firstTitle.addEventListener('click', event => {
+    console.log(`clicked`);
+    firstTitle.style.color ="red";
+    firstTitle.style.transition = 'color 0.5s';
+    event.preventDefault();
+});
+
+// FOCUS 
+
+const navFocus = document.querySelector('.nav-link');
+
+navFocus.addEventListener('focus', event => {
+    console.log ('focused');
+    navFocus.style.color = "purple";
+});
+
+
